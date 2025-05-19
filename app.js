42c290335a292c44c0e510be153891505ae67d54
Hongyu Yan <133737661+Markyan04@users.noreply.github.com>
2025-05-19 18:50:56 +0800

    feat(backend core): Route Management
    - Create routes directory to manage routers

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const aboutRouter = require('./routes/about');
+const quizRouter = require('./routes/quiz');
 
 const app = express();
 
@@ -21,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/quiz', quizRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
